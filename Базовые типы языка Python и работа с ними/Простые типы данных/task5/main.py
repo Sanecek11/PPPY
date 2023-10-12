1c322df520695b8492ad2e43b6dd9ca0b03cea55
Sanecek11 <alexandr.prosto.pro@mail.ru>
2023-10-12 19:44:21 +0300

    init commit

--- Базовые типы языка Python и работа с ними/Простые типы данных/task5/main.py
+++ Базовые типы языка Python и работа с ними/Простые типы данных/task5/main.py
@@ -2,12 +2,12 @@ speed = 4096  # скорость передачи данных в байтах/c
 time = 120  # время в минутах затраченное на скачивание игры
 cost = 0.125  # стоимость за один килобайт
 
-speed_in_kb_in_sec = 4096 // 1024  # TODO перевести скорость скачивания из байт в килобайты, если 1 килобайт = 1024 байта
+speed_in_kb_in_sec = 4096.0 // 1024.0  # TODO перевести скорость скачивания из байт в килобайты, если 1 килобайт = 1024 байта
 time_in_sec = 120 * 60  # TODO перевести время в секунды
 
 free_traffic = 500  # количество бесплатного трафика
-file_size = speed_in_kb_in_sec * 120  # TODO размер файла
-total_coast = (speed_in_kb_in_sec * 0.125) - 500   # TODO стоимость файла
+file_size = speed_in_kb_in_sec * time_in_sec  # TODO размер файла
+total_coast = (file_size - free_traffic) * cost  # TODO стоимость файла
 
 print('Размер файла в килобайтах =', file_size)
 print('За трафик придется заплатить:', total_coast)
