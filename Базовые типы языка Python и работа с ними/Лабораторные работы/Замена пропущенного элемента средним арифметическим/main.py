e4f633a7eb8e9cc451039c0a69d0a72af95e353a
Twinkie <alexandr.prosto.pro@mail.ru>
2023-10-18 14:57:37 +0300

    Выполненная первая ЛР

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
@@ -1,5 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-
-print("Измененный список:", ...)
+numbers[4] = 0
+sum_num = sum(numbers)
+len_num = len(numbers)
+avg_num = sum_num / len_num
+numbers[4] = avg_num
+print("Измененный список:", numbers)
