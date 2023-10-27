62526f0995e834c3712d7808525258d749d53206
Twinkie <alexandr.prosto.pro@mail.ru>
2023-10-27 16:55:44 +0300

    commit

--- Условия и циклы/Самостоятельная работа/task1/main.py
+++ Условия и циклы/Самостоятельная работа/task1/main.py
@@ -2,6 +2,9 @@ src = not False and True or False and not True
 
 # TODO расписать упрощение выражения
 
-result = ...  # TODO подставить результат выражения
+result = True  # TODO подставить результат выражения
 
 print(src == result)
+# and = *
+# not = противоположное
+# or =
--- Условия и циклы/Самостоятельная работа/task4/main.py
+++ Условия и циклы/Самостоятельная работа/task4/main.py
@@ -1,2 +1,7 @@
 # TODO завести счетчик для произведения
 # TODO посчитать произведение чисел
+num = 1
+num_l = 11
+for number in range(1, num_l):
+    num *= number
+print(num)
