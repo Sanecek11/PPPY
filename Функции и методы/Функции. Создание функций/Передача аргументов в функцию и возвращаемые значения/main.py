3e65f844f95696eaff3fe8fe09fb7605a1313aaf
Twinkie <alexandr.prosto.pro@mail.ru>
2023-10-29 19:24:24 +0300

    commit

--- Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
+++ Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
@@ -1,4 +1,11 @@
 # TODO Напишите функцию `multiply_numbers`
-
+def multiply_numbers(a, b):
+    return a * b
+product = multiply_numbers(2, 3)
+product_1 = multiply_numbers(4, 5)
+product_2 = multiply_numbers(6, 7)
+print(product)
+print(product_1)
+print(product_2)
 
 # TODO Перемножте с помощью функции `multiply_numbers` при пары чисел и распечатайте результаты
