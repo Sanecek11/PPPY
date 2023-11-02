839cc9cca6419ae41de6b9238c5b3db9205ff009
Sanecek11 <alexandr.prosto.pro@mail.ru>
2023-11-02 21:27:32 +0300

    init commit

--- Функции и методы/Практические задания/Поиск натуральных делителей числа/main.py
+++ Функции и методы/Практические задания/Поиск натуральных делителей числа/main.py
@@ -1,6 +1,10 @@
 def get_list_number_divisors(number):
     ...  # TODO Найдите список делителей числа number
-
+    list_of_dev = []
+    for num in range(1, number + 1):
+        if number % num == 0:
+           list_of_dev.append(num)
+    return list_of_dev
 
 print(get_list_number_divisors(23))
 print(get_list_number_divisors(2 ** 16))
