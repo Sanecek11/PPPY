f3108210cc92a8e051e518c596016df8c554897d
Twinkie <alexandr.prosto.pro@mail.ru>
2023-11-11 14:21:08 +0300

    commit

--- Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование RGB цветов/main.py
+++ Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование RGB цветов/main.py
@@ -1,7 +1,6 @@
 def convert_to_uppercase(colors: list[str]) -> list[str]:
     """Функция для преобразования каждого цвета в верхний регистр."""
-    return ...  # TODO Преобразовать цвета в верхний регистр
-
+    return [color.upper() for color in colors]# TODO Преобразовать цвета в верхний регистр
 
 if __name__ == '__main__':
     colors_list = ['#ff0000', '#00ff00', '#0000ff']
--- Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование десятичных чисел в шестнадцатеричные/main.py
+++ Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование десятичных чисел в шестнадцатеричные/main.py
@@ -1,5 +1,8 @@
 # TODO Напишите функцию decimal_to_hex
-
+def decimal_to_hex() -> dict[int, str]:
+    return {k: hex(k) for k in range(16)}
 
 if __name__ == '__main__':
     ...  # TODO Распечатайте словарь с десятичными и шестнадцатеричными числами
+converted_dict = decimal_to_hex()
+print(converted_dict)
